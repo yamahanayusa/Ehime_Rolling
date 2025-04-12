18882fbec5d4328854ddaff023ae06e029d64ea8
anoyasu <CA01244001@st.kawahara.ac.jp>
2025-04-12 17:33:12 +0900

    stage回転の修正

--- GameTemplate/Game/Stage.cpp
+++ GameTemplate/Game/Stage.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "Stage.h"
-
+#include "Player.h"
 
 Stage::Stage()
 {
@@ -36,17 +36,52 @@ void Stage::Update()
 
 void Stage::Rotation()
 {
-
+	Matrix mBias, mRot, mBiasInv, mFinal;
+	// 背景をプレイヤー空間に移動させる行列を計算する
+	if (m_player == nullptr) {
+		m_player = FindGO<Player>("player");
+	}
+	if (m_player == nullptr) {
+		return;
+	}
+	
 	// 背景の回転
 	//左右方向の傾き
-	addRot.SetRotationZ(g_pad[0]->GetLStickXF() * 0.005f);
-	m_bgRotation.Multiply(addRot);
-	m_bgObject.GetBody()->SetPositionAndRotation(Vector3::Zero, m_bgRotation);
+	Vector3 forwardXZ = g_camera3D->GetForward();
+	forwardXZ.y = 0.0f;
+	forwardXZ.Normalize();
+	addRot.SetRotation(forwardXZ, g_pad[0]->GetLStickXF() * -0.005f);
+	// プレイヤーの位置が原点に来るように背景を動かす行列を作成する
+	mBias.MakeTranslation(m_player->m_ballPosition * -1.0f);
+	mBiasInv = mBias;
+	// 動かした背景をもとに戻す行列を作成
+	mBiasInv.Inverse();
+	// 追加で回転させる行列を作成
+	mRot.MakeRotationFromQuaternion(addRot);
+	// 移動させた背景を回転させる
+	mFinal.Multiply(mBias, mRot);
+	// 背景の位置を戻す
+	mFinal.Multiply(mFinal, mBiasInv);
 
+	// 最終的に出来上がった行列から回転クォータニオンを作る
+	addRot.SetRotation(mFinal);
+
+	m_bgRotation.Multiply(addRot);
+	
 	//上下方向の傾き
-	addLot.SetRotationX(g_pad[0]->GetLStickYF() * -0.005f);
+	Vector3 rightXZ = g_camera3D->GetRight();
+	rightXZ.y = 0.0f;
+	rightXZ.Normalize();
+	addLot.SetRotation(rightXZ, g_pad[0]->GetLStickYF() * 0.005f);
+
+	mRot.MakeRotationFromQuaternion(addLot);
+	mFinal.Multiply(mBias, mRot);
+	mFinal.Multiply(mFinal, mBiasInv);
+	addLot.SetRotation(mFinal);
+
 	m_bgRotation.Multiply(addLot);
 	//
+	m_bgObject.GetBody()->SetPositionAndRotation(Vector3::Zero, m_bgRotation);
 	m_bgModelRendedr.SetRotation(m_bgRotation);
 	//モデルレンダーのアップデート
 	m_bgModelRendedr.Update();
