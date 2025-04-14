6860117a02d727de2672416d3b6ca76334466cdf
yamanakakousyou <CA01244030@st.kawahara.ac.jp>
2025-04-14 17:59:57 +0900

    GameClearの画像にスコアを表示

--- GameTemplate/Game/GameClear.h
+++ GameTemplate/Game/GameClear.h
@@ -1,5 +1,7 @@
 #pragma once
 class Game;
+class Score;
+class Chest;
 //ゲームクリア
 class GameClear :public IGameObject
 {
@@ -12,9 +14,14 @@ public:
 	void Update();
 	//描画処理
 	void Render(RenderContext& rc);
+	//
+	void TortalScore();
 
 	//メンバ変数
 	SpriteRender m_spriteRender; //スプライトレンダー
+	FontRender m_fontRender; //
 	Game* m_game;
+	Score* m_score;
+	Chest* m_chest;
 };
 
