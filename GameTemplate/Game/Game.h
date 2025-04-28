1ea62282eba59312db808c569bd4e184d9883f7f
yamahanayusa <CA01244031@st.kawahara.ac.jp>
2025-04-28 12:14:20 +0900

    コメントの追加

--- GameTemplate/Game/Game.h
+++ GameTemplate/Game/Game.h
@@ -39,19 +39,19 @@ public:
 private:
 	LevelRender m_levelRender;
 
-	Title* m_title;
-	Chest* m_chest;
-	Timer* m_timer;
-	Mikan* m_mikan[4];
-	TimeOver* m_timeOver;
-	GameOver* m_gameOver;
-	GameClear* m_gameClear;
-	Score* m_score;
-	Stage* m_stage = nullptr;
-	Player* m_player = nullptr;
-	GameCamera* m_gameCamera = nullptr;
-	IceFloor* m_iceFloor = nullptr;
-	Vector3 m_pos;
-	int m_resultTime = 0;
+	Title*					m_title;
+	Chest*					m_chest;
+	Timer*					m_timer;
+	Mikan*					m_mikan[4];
+	TimeOver*				m_timeOver;
+	GameOver*				m_gameOver;
+	GameClear*				m_gameClear;
+	Score*					m_score;
+	Stage*					m_stage	= nullptr;
+	Player*					m_player = nullptr;
+	GameCamera*				m_gameCamera = nullptr;
+	IceFloor*				m_iceFloor = nullptr;
+	Vector3					m_pos;
+	int						m_resultTime = 0;
 };
 
