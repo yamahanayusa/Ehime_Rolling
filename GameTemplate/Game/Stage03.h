7d71fc0869699858719123b197d1811004b86940
anoyasu <CA01244001@st.kawahara.ac.jp>
2025-07-08 15:40:09 +0900

    Stageの継承、オーバーライド

--- GameTemplate/Game/Stage03.h
+++ GameTemplate/Game/Stage03.h
@@ -1,46 +1,37 @@
 #pragma once
 
-class Player;
-class Transform;
-class CountDown;
+#include "BaseStage.h" // BaseStage を継承するのでインクルード
+#include "Transform.h" // Transform を独自に持つので、ここでインクルード
 
-class Stage03 : public IGameObject
+class Stage03 : public BaseStage
 {
 public:
-	Stage03();
-	~Stage03();
-	bool Start();
-	void Update();
-	void Rotation();
-	void Render(RenderContext& rc);
+    // コンストラクタで、基底クラスのコンストラクタにモデルパスを渡す
+    Stage03();
+    // デストラクタ: 独自の m_transform を削除
+    ~Stage03();
 
-	Transform* GetTransform()
-	{
-		return m_transform;
-	}
+    // 基底クラスのメソッドをオーバーライドし、独自の m_transform を渡す
+    bool Start() override;
+    void Update() override;
+    void Rotation(Transform* transform) override;
+    void Render(RenderContext& rc) override; // Render も Transform を渡す形にOverride
 
-	Matrix GetWorldMatrix() const
-	{
-		return m_modelRender.GetWorldMatrix(0);
-	}
+    // 独自の GetTransform() を実装
+    Transform* GetTransform()
+    {
+        return m_transform;
+    }
 
-	Transform*		m_transform		= nullptr;
+    Matrix GetWorldMatrix() const
+    {
+        // 独自の Transform からワールド行列を取得
+        if (m_transform) {
+            return m_modelRender.GetWorldMatrix(0);
+        }
+        return Matrix::Identity; // エラー時のデフォルト
+    }
+    Transform* m_transform = nullptr;
+private: // 各ステージが独自の Transform を持つ
 
-private:
-	ModelRender			m_modelRender;
-	PhysicsStaticObject m_object;
-
-	//大きさ
-	Vector3			m_scale = Vector3::One;
-
-	//回転を設定する
-	Quaternion		addRot	= Quaternion::Identity;
-	Quaternion		addLot	= Quaternion::Identity;
-
-	Player*			m_player	= nullptr;
-	CountDown*		m_countDown = nullptr;
 };
-
-
-
-
